2a3c531ea99b282ce9dd470fd25d1bd60bed6f1c
Rahul Kalaria <rahul.universityid@gmail.com>
2023-09-20 17:51:00 +0200

    Added fade animation to AI answer

--- exact-semantic-search-frontend/src/components/AiResult.tsx
+++ exact-semantic-search-frontend/src/components/AiResult.tsx
@@ -8,15 +8,6 @@ type Props = {
 };
 
 export const AiResult: React.FC<Props> = (props) => {
-	const placeholder = (
-		<>
-			<div className="skeleton">
-				Generating an answer for you...
-				<SkeletonText mt="0" noOfLines={10} spacing="4" skeletonHeight="2" />
-			</div>{" "}
-		</>
-	);
-
 	const answer = (
 		<>
 			{" "}
@@ -25,11 +16,29 @@ export const AiResult: React.FC<Props> = (props) => {
 			<span>If you would like to know more, please refer to the documents below.</span>
 		</>
 	);
+	const placeholder = (
+		<>
+			<div className="skeleton">
+				{props.isLoadingAiAnswer === "loading" && "Generating an answer for you..."}
+				<SkeletonText
+					mt="0"
+					noOfLines={10}
+					spacing="4"
+					skeletonHeight="2"
+					speed={0.5}
+					isLoaded={props.isLoadingAiAnswer === "success" ? true : false}
+					fadeDuration={1.2}
+				>
+					{answer}
+				</SkeletonText>
+			</div>{" "}
+		</>
+	);
 
 	return (
 		<div className="ai-result-container box">
-			{props.isLoadingAiAnswer === "success" && answer}
-			{props.isLoadingAiAnswer === "loading" && placeholder}
+			{props.isLoadingAiAnswer !== "error" && placeholder}
+			{props.isLoadingAiAnswer === "error" && <>Sorry, I could not generate your answer :(</>}
 		</div>
 	);
 };
